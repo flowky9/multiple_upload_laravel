b089fd1497743f051c9b26f863ec99c4636a653a
nauvalshidqi <nauvals78@gmail.com>
2018-04-11 23:52:21 +0700

    First Commit

---
+++ resources/views/upload_form.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+    </head>
+    <body>
+
+    @if (count($errors) > 0)
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    @endif
+
+<form action="/upload" method="post" enctype="multipart/form-data">
+    {{ csrf_field() }}
+    Product name:
+    <br />
+    <input type="text" name="name" />
+    <br /><br />
+    Product photos (can attach more than one):
+    <br />
+    <input type="file" name="photos[]" multiple />
+    <br /><br />
+    <input type="submit" value="Upload" />
+</form>
+    </body>
+</html>
